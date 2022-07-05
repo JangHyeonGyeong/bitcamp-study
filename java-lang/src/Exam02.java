47e368748c76667dd39404e2a37cb464d0afff15
JangHyeonGyeong <jhg_2020@naver.com>
2022-07-05 17:16:26 +0900

    수업종료

---
+++ java-lang/src/Exam02.java
@@ -0,0 +1,3 @@
+public class Exam02 {}
+
+// public 사용시 클래스명과 파일명은 같아야함.
---
+++ java-lang/src/Exam03.java
@@ -0,0 +1,12 @@
+public class Exam03 {
+  public static void main(String[] args) {
+    System.out.println("Hello!");
+  }
+}
+
+/*public class Exam03 {
+  void m1() {
+    System.out.println("Hello!");
+  }
+} 
+컴파일은 가능, 실행에 오류 확인 */
