c8f0c2922ed70334550bcfed5e72326178d5b5bd
JangHyeonGyeong <jhg_2020@naver.com>
2022-09-01 09:41:11 +0900

    09.01

--- rocspaper/src/com/rocspaper/rocrspaper.java
+++ rocspaper/src/com/rocspaper/rocrspaper.java
@@ -10,63 +10,55 @@ public class rocrspaper {
     Scanner scanner = new Scanner(System.in); // 키보드입력받ㅈㅇ
 
     while(true) {
+
       printMenu(); // 메뉴
 
       int user =  inputChoice(scanner); //유저 선택
-
-      // computer();
 
       if(user == 0) { // 0번 종료
         System.out.println("종료합니다");
         break;
       }
 
+      computer();
 
-      // 승부
-
-      Random random = new Random();
-      int ran ;
-      ran = random.nextInt(3); // 0~2까지 랜덤수
-
-      System.out.println("computer : " + ran);
-
-      if (ran == 0) {
-        System.out.println("컴퓨터의 " + ran +" 은 가위입니다");
-      }else if(ran ==1) {
-        System.out.println("컴퓨터의 "  + ran +" 은 바위입니다");
-      }else 
-        System.out.println("컴퓨터의 "  + ran +" 는보입니다");
-
-
-      if(user == 1) {
-        if (ran ==0)
-          System.out.println(" 비겼습니다. ");
-        else if(ran ==1)
-          System.out.println(" 컴퓨터가 이겼습니다");
-
-        else System.out.println("당신이 이겼습니다.");
-      } 
+      fi();
 
 
 
-      if(user == 2) {
-        if (ran ==0)
-          System.out.println("  ");
-        else if(ran ==1)
-          System.out.println(" 비겼습니다");
-
-        else System.out.println("컴퓨터가 이겼습니다.");
-      } 
 
 
-      if(user == 3) {
-        if (ran ==0)
-          System.out.println(" 컴퓨터가 이겼습니다. ");
-        else if(ran ==1)
-          System.out.println(" 당신이 이겼습니다");
 
-        else System.out.println("비겼습니다.");
-      } 
+
+
+
+      //      if(user == 1) {
+      //        if (computer() ==0)
+      //          System.out.println(" 비겼습니다. ");
+      //        else if(computer() ==1)
+      //          System.out.println(" 컴퓨터가 이겼습니다");
+      //
+      //        else System.out.println("당신이 이겼습니다.");
+      //      } 
+      //
+      //      if(user == 2) {
+      //        if (computer() ==0)
+      //          System.out.println("  ");
+      //        else if(computer() ==1)
+      //          System.out.println(" 비겼습니다");
+      //
+      //        else System.out.println("컴퓨터가 이겼습니다.");
+      //      } 
+      //
+      //
+      //      if(user == 3) {
+      //        if (computer() ==0)
+      //          System.out.println(" 컴퓨터가 이겼습니다. ");
+      //        else if(computer() ==1)
+      //          System.out.println(" 당신이 이겼습니다");
+      //
+      //        else System.out.println("비겼습니다.");
+      //      } 
 
 
 
@@ -101,22 +93,59 @@ public class rocrspaper {
   }
 
 
-  //  private static void computer() {
-  //
-  //    Random random = new Random();
-  //    int ran ;
-  //    ran = random.nextInt(3); // 0~2까지 랜덤수
-  //
-  //    System.out.println("computer : " + ran);
-  //    if (ran == 0) {
-  //      System.out.println(ran +": 가위입니다");
-  //    }else if(ran ==1) {
-  //      System.out.println(ran +": 바위입니다");
-  //    }else 
-  //      System.out.println(ran +": 보입니다");
-  //
-  //
-  //  }
+  private static  int computer() {
 
+    Random random = new Random();
+    int ran ;
+    ran = random.nextInt(3); // 0~2까지 랜덤수
+
+    System.out.println("computer : " + ran);
+    if (ran == 0) {
+      System.out.println(ran +": 가위입니다");
+    }else if(ran ==1) {
+      System.out.println(ran +": 바위입니다");
+    }else 
+      System.out.println(ran +": 보입니다");
+
+    return ran;
+
+  }
+
+
+  public static void fi() {
+
+    while (true) {
+
+      switch (inputChoice(null)) {
+        case 1: 
+          if (computer() ==0)
+            System.out.println(" 비겼습니다. ");
+          else if(computer() ==1)
+            System.out.println(" 컴퓨터가 이겼습니다");
+          else System.out.println("당신이 이겼습니다.");
+          // break;
+
+
+        case 2: 
+          if (computer() ==0)
+            System.out.println("  ");
+          else if(computer() ==1)
+            System.out.println(" 비겼습니다");
+          else System.out.println("컴퓨터가 이겼습니다.");
+          //  break;
+
+        case 3: 
+          if (computer() ==0)
+            System.out.println(" 컴퓨터가 이겼습니다. ");
+          else if(computer() ==1)
+            System.out.println(" 당신이 이겼습니다");
+          else System.out.println("비겼습니다.");
+
+          //   break;
+
+      }
+    }
+
+  }
 
 }
